ba03a8633848211a29cbb9a076d00f0714ad8ca1
bombanira <bombanira@yahoo.co.jp>
2020-08-02 19:05:46 +0900

    Manager Change

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -11,12 +11,9 @@ public class Manager: MonoBehaviour
     public Timer ElapsedTime = new Timer(1000); // 1秒ごとに関数を実行
     public int TermCounter { get; private set; } = 1;
     #endregion
-    Vector2 kato_farst_pos = new Vector2(0.0f, 10.0f);
-    Vector2 kato_size = new Vector2(3.0f, 3.0f);
 
-    Vector2 syokisyoki_farst_pos = new Vector2(0.0f, -9.0f);
-    Vector2 syokisyoki_size = new Vector2(0.6f, 0.6f);
-    Vector2 syokisyoki_collider = new Vector2(-0.21f,0.05f);
+    public GameObject Kato { get; private set; }
+    public GameObject Syokisyoki { get; private set; }
 
     public static Manager Instance { get; private set; } = new Manager();
     private Manager()
@@ -37,26 +34,41 @@ public class Manager: MonoBehaviour
 
     private void Awake()
     {
-        GameObject kato = new GameObject("kato_prefab");
-        kato.AddComponent<Transform>();
-        kato.AddComponent<SpriteRenderer>();
-        kato.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("kato");
-        kato.transform.position = kato_farst_pos;
-        kato.transform.localScale = kato_size;
+        CreateKato();
+        CreateSyokisyoki();
+    }
+    private void CreateKato()
+    {
+        Kato = new GameObject("kato_prefab");
+        Vector2 kato_farst_pos = new Vector2(0.0f, 10.0f);
+        Vector2 kato_size = new Vector2(3.0f, 3.0f);
 
-        GameObject syokisyoki = new GameObject("syoki_prefab");
-        syokisyoki.AddComponent<Transform>();
-        syokisyoki.AddComponent<SpriteRenderer>();
-        syokisyoki.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("syokisyoki");
-        syokisyoki.transform.position = syokisyoki_farst_pos;
-        syokisyoki.transform.localScale = syokisyoki_size;
-        syokisyoki.AddComponent<CircleCollider2D>();
-        CircleCollider2D syoki_collider = syokisyoki.GetComponent<CircleCollider2D>();
+        Kato.AddComponent<Transform>();
+        Kato.AddComponent<SpriteRenderer>();
+        Kato.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("kato");
+        Kato.transform.position = kato_farst_pos;
+        Kato.transform.localScale = kato_size;
+    }
+    private void CreateSyokisyoki()
+    {
+        Vector2 syokisyoki_farst_pos = new Vector2(0.0f, -9.0f);
+        Vector2 syokisyoki_size = new Vector2(0.6f, 0.6f);
+        Vector2 syokisyoki_collider = new Vector2(-0.21f, 0.05f);
+
+        Syokisyoki = new GameObject("syoki_prefab");
+        Syokisyoki.AddComponent<Transform>();
+        Syokisyoki.AddComponent<SpriteRenderer>();
+        Syokisyoki.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("syokisyoki");
+        Syokisyoki.transform.position = syokisyoki_farst_pos;
+        Syokisyoki.transform.localScale = syokisyoki_size;
+        Syokisyoki.AddComponent<CircleCollider2D>();
+        CircleCollider2D syoki_collider = Syokisyoki.GetComponent<CircleCollider2D>();
         syoki_collider.radius = 5;//syokisyokiの当たり判定サイズ
         syoki_collider.offset = syokisyoki_collider;//当たり判定オフセット 名前が微妙
-        syokisyoki.AddComponent<Rigidbody2D>();
-        syokisyoki.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        Syokisyoki.AddComponent<Rigidbody2D>();
+        Syokisyoki.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
     }
+
 
     void Update()
     {
