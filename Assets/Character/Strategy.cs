00185fe7a4c37aa3115059096dbc6a0752b5f824
holmesyf <holmesyf@gmail.com>
2020-08-06 19:46:49 +0900

    add strategy class and order class in character

---
+++ Assets/Character/Strategy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Strategy : MonoBehaviour
+{
+    protected Order order;
+    protected Strategy()
+    {
+        this.order = new Order();
+    }
+    protected Vector2 pos;
+    public abstract Order getOrder();
+    public virtual void set_pos(Vector2 pos) {
+        this.pos = pos;
+    }
+}
