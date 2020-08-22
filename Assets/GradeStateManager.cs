1d0f4fac59efb361a5ad2d55c43927b0e5582ae0
bombanira <bombanira@yahoo.co.jp>
2020-08-22 15:02:04 +0900

    merge

--- Assets/GradeStateManager.cs
+++ Assets/GradeStateManager.cs
@@ -7,19 +7,17 @@ public class GradeStateManager
     public static string GetCurrentGrade(int term)
     {
         string result;
-        if(term > 15)
+        if (term < 12)
         {
-            throw new System.Exception();
-        }
-        else if (term  < 12)
-        { 
-            result = "本科 " + (term/2).ToString() + "年";
+            result = "本科 " + (term / 2).ToString() + "年";
 
         }
-        else
+        else if (term < 16)
         {
-            result = "専攻科 " + ((term-10)/2).ToString() + "年";
+            result = "専攻科 " + ((term - 10) / 2).ToString() + "年";
         }
+        else
+            throw new System.Exception();
 
         if (term % 2 == 0)
         {
@@ -29,6 +27,27 @@ public class GradeStateManager
             result += " 後期";
 
         return result ; 
+    }
+
+    public static int GetCreditLimit(int term)
+    {
+        if (term / 2 == 1)
+            return 3;
+        else if (term / 2 == 2)
+            return 4;
+        else if (term / 2 == 3)
+            return 5;
+        else if (term / 2 == 4)
+            return 6;
+        else if (term / 2 == 5)
+            return 2;
+        else if (term / 2 == 6)
+            return 7;
+        else if (term / 2 == 7)
+            return 3;
+        else
+            throw new System.Exception();
+
     }
 }
 
